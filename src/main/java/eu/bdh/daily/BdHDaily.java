ac4877029461be71a05ddec2bd5864992e635a34
Florian <eckhard.florian@student.dhbw-karlsruhe.de>
2018-09-14 18:00:37 +0200

    Add ConfigHelper Beginnings with config creation in main class

--- src/main/java/eu/bdh/daily/BdHDaily.java
+++ src/main/java/eu/bdh/daily/BdHDaily.java
@@ -1,12 +1,21 @@
 package eu.bdh.daily;
 
+import eu.bdh.daily.util.ConfigHelper;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BdHDaily extends JavaPlugin {
 
+    private static BdHDaily plugin;
+
     @Override
     public void onEnable() {
-        super.onEnable();
+        //Define Plugin to Variable
+        plugin = this;
+        //Erzeugung der ConfigFiles
+        ConfigHelper configHelper = new ConfigHelper(plugin,"lang");
+        configHelper.createDefaultConfig();
+        //TODO Abspeicherung der Configuration
+        configHelper.createYamlFile();
     }
 
     @Override
@@ -14,4 +23,9 @@ public class BdHDaily extends JavaPlugin {
         super.onDisable();
     }
 
+
+    //Getter and Setter
+    public static BdHDaily getPlugin() {
+        return plugin;
+    }
 }
---
+++ src/main/java/eu/bdh/daily/util/ConfigHelper.java
@@ -0,0 +1,107 @@
+/*
+ * BdHRDaily, a Minecraft daily event plugin
+ * Copyright (C) Clive, ReLuLu
+ * Copyright (C) daily team and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package eu.bdh.daily.util;
+
+import eu.bdh.daily.BdHDaily;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author Florian
+ * Config Helper zur Anlage der benötigen Config Files
+ * Aktuell: Config und Language
+ */
+public class ConfigHelper {
+
+    private static final Logger log = Logger.getLogger(ConfigHelper.class.getName());
+    private String configName;
+    private BdHDaily plugin;
+
+    /**
+     * Konsturktur zur Erzeugung des ConfigHelper Klasse
+     * @param configName Parameter wird benötigt, wenn nicht die Standard Config File sondern eine andere Config File erzeugt werden soll.
+     * @param plugin BdHDaily Objekt zum Zugriff auf das Plugin
+     */
+    public ConfigHelper(BdHDaily plugin, String configName){
+        this.configName = configName;
+        this.plugin = plugin;
+    }
+
+    /**
+     * Nur zur Erstellung der Default Config nutzbar.
+     * Konsturktur zur Erzeugung des ConfigHelper Klasse
+     * @param plugin BdHDaily Objekt zum Zugriff auf das Plugin
+     */
+    public ConfigHelper(BdHDaily plugin){
+        this.plugin = plugin;
+        this.configName = "error";
+    }
+
+    public void createDefaultConfig() {
+        File dataFolder = plugin.getDataFolder();
+        try {
+            if (!dataFolder.exists()) {
+                boolean result = dataFolder.mkdirs();
+                if (!result){
+                    Bukkit.getPluginManager().disablePlugin(BdHDaily.getPlugin());
+                    log.log(Level.SEVERE,"Config file not creatable!");
+                }
+            }
+            File file = new File(dataFolder, "config.yml");
+            if (!file.exists()) {
+                log.info("Config.yml not found, creating!");
+                plugin.saveDefaultConfig();
+            } else {
+                log.info("Config.yml found, loading!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public FileConfiguration createYamlFile(){
+        File file = new File(plugin.getDataFolder(), configName + ".yml");
+
+        if (!file.exists()) {
+            boolean result = file.getParentFile().mkdirs();
+            if (!result){
+                Bukkit.getPluginManager().disablePlugin(BdHDaily.getPlugin());
+                log.log(Level.SEVERE,"Config file not creatable!");
+            }
+            plugin.saveResource(configName + ".yml", false);
+        }
+
+        FileConfiguration fileConfiguration = new YamlConfiguration();
+
+        try {
+            fileConfiguration.load(file);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+        return fileConfiguration;
+    }
+}
